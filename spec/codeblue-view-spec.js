b9738b4d792bd3034dc2b8f5c3669d220951ddf6
blueedgetechno <blueedgetechno@gmail.com>
2020-08-10 21:54:11 +0530

    Codeforces

---
+++ spec/codeblue-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeblueView from '../lib/codeblue-view';
+
+describe('CodeblueView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
